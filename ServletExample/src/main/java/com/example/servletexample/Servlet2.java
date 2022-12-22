73f56eb2cc2083a6aa5d655b60a5cfbc51e1b3ff
JasinBeadini <jas.beadini@gmail.com>
2022-12-22 11:27:51 +0100

    update

--- ServletExample/src/main/java/com/example/servletexample/Servlet2.java
+++ ServletExample/src/main/java/com/example/servletexample/Servlet2.java
@@ -19,14 +19,19 @@ public class Servlet2 extends HttpServlet {
     private static final long serialVersionUID=1L;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String ciao="prova.jsp";
+        String page="prova.jsp";
         String nome="Jasin";
         request.setAttribute("name", nome);
-        RequestDispatcher view = request.getRequestDispatcher(ciao);
+        RequestDispatcher view = request.getRequestDispatcher(page);
         view.forward(request, response);
     }
 
-    public void destroy() {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String page="url.jsp";
+        String nome2= request.getParameter("nome");
+        request.setAttribute("nome", nome2);
+        RequestDispatcher view = request.getRequestDispatcher(page);
+        view.forward(request, response);
     }
 
 
--- ServletExample/src/main/webapp/prova.jsp
+++ ServletExample/src/main/webapp/prova.jsp
@@ -1,3 +1,4 @@
+<jsp:useBean id="nome" scope="request" type=""/>
 
 <%--
   Created by IntelliJ IDEA.
@@ -57,7 +58,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<h1><a href="hello-servlet2">Prova</a></h1>
 ${nome}
-<a href="hello-servlet2">Prova</a>
+
 </body>
 </html>
